31399495f23893dfd3542a34e8ac95b78e1f0a62
Lucas Nunes <lucasnunespinto48@gmail.com>
2023-06-08 22:06:09 -0300

    Atualização

---
+++ texto_formatado.py
@@ -0,0 +1,10 @@
+Nome = input('Qual é o seu nome? ')
+Idade = input('Qual é a sua idade em anos? ')
+Idade = str(Idade)
+Cidade = input('Qual cidade está morando atualmente? ')
+
+texto = f'o {Nome} tem {Idade} anos de idade e mora na cidade de {Cidade}'
+
+print (texto.capitalize())
+
+# O Lucas tem 24 anos de idade e mora na cidade de Anápolis
